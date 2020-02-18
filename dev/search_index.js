03de362676b2bfcf446f6990092527b487fdd9ea
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-02-18 19:30:08 +0000

    build based on 983f0cb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#julia_test-1","page":"Home","title":"julia_test","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"This is my julia_test package","category":"page"}]
+}
