983f0cb437e16834973a7966e2eba1c39f11d44a
Zhaoyi Shen <pkuszy@gmail.com>
2020-02-18 11:28:42 -0800

    add foo

--- src/julia_test.jl
+++ src/julia_test.jl
@@ -2,4 +2,6 @@ module julia_test
 
 greet() = print("Hello World!")
 
+foo() = 1
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,7 @@
 using Test
+using julia_test
 
 @testset "my test" begin
     @test 1+1==2
+    @test julia_test.foo() == 1
 end
